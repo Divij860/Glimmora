6cdebd553c89b7146bee1b29af41045ff224410b
Divij <divijp.ai20@sahyadri.edu.in>
2025-05-02 17:54:46 +0530

    fifth commit

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,11 +1,16 @@
-import Jewel1 from './jewel1.jpg';
-import Jewel2 from './jewel2.jpg';
-import Jewel3 from './jewel3.jpg';
-import Jewel4 from './jewel4.jpg';
-import Jewel5 from './jewel5.jpg';
-import Jewel6 from './jewel6.jpg';
-import Jewel7 from './jewel7.jpg';
+import Jewel1 from './jewel1.png';
+import Jewel2 from './jewel2.png';
+import Jewel3 from './jewel3.png';
+import Jewel4 from './jewel4.png';
+import Jewel5 from './jewel5.png';
+import Jewel6 from './jewel6.png';
+import Jewel7 from './jewel7.png';
+import Jewel8 from './jewel8.png';
+import Jewel9 from './jewel9.png';
+import Jewel10 from './jewel10.png';
+import Jewel11 from './jewel11.png';
+import Jewel12 from './jewel12.png';
 import Logo from './sketch.png';
 
 
-export {Logo,Jewel1, Jewel2, Jewel3, Jewel4, Jewel5, Jewel6, Jewel7};
+export {Logo,Jewel1, Jewel2, Jewel3, Jewel4, Jewel5, Jewel6, Jewel7, Jewel8, Jewel9, Jewel10, Jewel11, Jewel12};
